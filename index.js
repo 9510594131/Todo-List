00b4b9ac9b49bc25f2377a91f3a582fe8aa4ac50
Tirth K. Sapariya <153990515+9510594131@users.noreply.github.com>
2025-01-04 17:27:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
-const PORT = 3000;
 
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
 const users = [];
 
 app.use(cors({
-    origin: 'http://yourfrontend.vercel.app', 
+    origin: 'https://yourfrontend.vercel.app',
     methods: ['GET', 'POST', 'DELETE'],
     credentials: true
 }));
 
-app.use(express.static(__dirname));
+app.use(express.json());
 
 app.post("/contact", (req, res) => {
     const userData = req.body;
-    console.log("Received user data:", userData);
-
     users.push(userData);
-
     res.json({
         message: "User data received successfully",
         data: users
@@ -30,13 +25,8 @@ app.post("/contact", (req, res) => {
 app.delete("/delete/:name", (req, res) => {
     const nameToDelete = req.params.name;
     const index = users.findIndex(user => user.name.toLowerCase() === nameToDelete.toLowerCase());
-
-    console.log(`Trying to delete user: ${nameToDelete}`);
-
     if (index !== -1) {
         users.splice(index, 1);
-        console.log("Users Array after deletion:", users);
-
         res.json({
             message: "User data deleted successfully",
             data: users
